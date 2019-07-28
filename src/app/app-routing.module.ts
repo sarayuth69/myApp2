36413a995326e386ea87fbc3a49e65a446cf57c5
Sarayuth69 <sarauthkrisor69@gmail.com>
2019-07-29 02:38:32 +0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'fooditems', loadChildren: './fooditems/fooditems.module#FooditemsPageModule' },
+  { path: 'insert', loadChildren: './insert/insert.module#InsertPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
